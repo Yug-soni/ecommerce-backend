f41d1c99ea950bfdb2e85b603e499d2638dc4979
Yug-soni <yugsoni8946842393@gmail.com>
2021-08-01 14:15:18 +0530

    switched from form login to http basic auth

--- src/main/java/com/yug/startup/security/config/WebSecurityConfig.java
+++ src/main/java/com/yug/startup/security/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .httpBasic();
     }
 
     @Override
---
+++ src/main/java/com/yug/startup/t/FakeData.java
@@ -0,0 +1,11 @@
+package com.yug.startup.t;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FakeData {
+    public int username;
+    public int password;
+}
